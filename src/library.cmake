acf2d187a617a81fe9f8d2f0cca50925cb2d73bd
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2025-03-15 17:34:20 +0100

    refactor: Move headers to `include` directory, fix dependees not able to see headers

--- src/library.cmake
+++ src/library.cmake
@@ -4,13 +4,18 @@ set( SERIAL_TRANSFER_DIR ${CMAKE_CURRENT_LIST_DIR} )
 
 add_library( SerialTransfer STATIC
     "${SERIAL_TRANSFER_DIR}/Packet.cpp"
-    "${SERIAL_TRANSFER_DIR}/Packet.h"
-    "${SERIAL_TRANSFER_DIR}/PacketCRC.h"
+    "${SERIAL_TRANSFER_INCLUDE_DIR}/Packet.h"
+    "${SERIAL_TRANSFER_INCLUDE_DIR}/PacketCRC.h"
     "${SERIAL_TRANSFER_DIR}/SerialTransfer.cpp"
-    "${SERIAL_TRANSFER_DIR}/SerialTransfer.h"
+    "${SERIAL_TRANSFER_INCLUDE_DIR}/SerialTransfer.h"
 )
 
 add_library( SerialTransfer::SerialTransfer ALIAS SerialTransfer )
+
+target_include_directories( SerialTransfer
+	PUBLIC
+		${SERIAL_TRANSFER_INCLUDE_DIR}
+)
 
 # pull in common dependencies
 target_link_libraries( SerialTransfer
